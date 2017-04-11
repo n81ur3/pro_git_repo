d23ae96d3368980a5b0f68746f89bc75892fadc4
Helmut Bierbaumer <helmut.bierbaumer@edu.fh-joanneum.at>
2017-04-11 08:19:20 +0200

    created the power method

--- Hello.java
+++ Hello.java
@@ -7,5 +7,16 @@ public class Hello {
 		System.out.println("Calculation 2 + 3: " + (2+3));
 		System.out.println("Modulo Operation 3243%7: " + (3243%7));
 		System.out.printf("Created by %s %s \n", name, last_name);
+		printPower(2, 8);
+	}
+
+	private static void printPower(int base, int exp) {
+		int result = 1;
+
+		for (int i = 0; i < exp; i++) {
+			result *= base;
+		}
+
+		System.out.println(base + " to the power of " + exp + " equals " + result);
 	}
 }
