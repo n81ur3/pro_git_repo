996ab1fd0b5e4c977642f5b0562105d3390661d3
Helmut Bierbaumer <helmut.bierbaumer@edu.fh-joanneum.at>
2017-04-11 07:59:52 +0200

    Included modulo operation in Hello.java

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
 		System.out.println("Calculation 2 + 3: " + (2+3));
+		System.out.println("Modulo Operation 3243%7: " + (3243%7));
 	}
 }
