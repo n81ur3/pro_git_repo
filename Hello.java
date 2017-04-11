ad260313a24f06f3ab94520f6490935e80bcb69e
Helmut Bierbaumer <helmut.bierbaumer@edu.fh-joanneum.at>
2017-04-11 08:05:40 +0200

    Included author information for Hello.java

--- Hello.java
+++ Hello.java
@@ -1,8 +1,10 @@
 public class Hello {
 
 	public static void main(String[] args) {
+		String name = "Helmut";
+		String last_name = "Bierbaumer";
 		System.out.println("Hello Git");
 		System.out.println("Calculation 2 + 3: " + (2+3));
-		System.out.println("Modulo Operation 434%8 = " + (434%8));
+		System.out.printf("Created by %s %s \n", name, last_name);
 	}
 }
